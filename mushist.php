586979aabbd32c0d0c349b034073e14a411cc620
daVinciManuel <mgmt.manuel02@gmail.com>
2025-05-20 10:51:41 +0200

    Project Start

---
+++ mushist.php
@@ -0,0 +1,42 @@
+<html>
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SPOTIFY - IES Leonardo Da Vinci - Alquiler Películas</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    </head>
+   
+
+   
+   <body>
+   
+
+     <div class="container ">
+        <!--Aplicacion-->
+		<div class="card border-success mb-3" style="max-width: 30rem;">
+		<div class="card-header">Consultar Historial - SPOTIFY Leonardo Da Vinci</div>
+		<div class="card-body">
+	<B>Nombre Cliente:</B>   <BR>
+	<B>Id Cliente:</B> <BR>
+	<B>Compañia:</B> <BR><BR>
+				 
+		
+	Fecha Desde: <input type='date' name='fechadesde' value='' size=10 placeholder="fechadesde" class="form-control">
+	Fecha Hasta: <input type='date' name='fechahasta' value='' size=10 placeholder="fechahasta" class="form-control">
+		
+		<BR>
+		<BR>		
+		<div>
+		    <input type="submit" value="Consultar Historial" name="Volver" class="btn btn-warning disabled">
+			<input type="submit" value="Volver" name="Volver" class="btn btn-warning disabled">
+		</div>		
+	</form>
+	<!-- FIN DEL FORMULARIO -->
+  </div>
+
+
+
+</body>
+</html>
+
---
+++ muswelcome.php
@@ -0,0 +1,37 @@
+<html>
+   
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SPOTIFY - IES Leonardo Da Vinci</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    </head>
+   
+    <body>
+   
+
+    <div class="container ">
+        <!--Aplicacion-->
+		<div class="card border-success mb-3" style="max-width: 30rem;">
+		<div class="card-header">Menú Usuario SPOTIFY Leonardo Da Vinci</div>
+		<div class="card-body">
+	<B>Nombre Cliente:</B>   <BR>
+	<B>Id Cliente:</B> <BR>
+	<B>Compañia:</B> <BR><BR>
+			
+		<!--Formulario con botones -->
+		<input type="button" value="Descargar Canciones" onclick="window.location.href=''" class="btn btn-warning disabled">
+		
+		<input type="button" value="Historial Facturas" onclick="window.location.href=''" class="btn btn-warning disabled">
+		
+		<input type="button" value="Consultar Factura" onclick="window.location.href=''" class="btn btn-warning disabled"><BR>
+		
+		  <BR><a href="">Cerrar Sesión</a>
+	</div>  
+	  
+	  
+     
+   </body>
+   
+</html>
